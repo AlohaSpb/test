c058d98c95017bb8c0f72080172270ccfca1069a
ar1stotel <londonhek111@gmail.com>
2026-09-17 00:39:48 +0400

    Use Vercel-compatible question shuffling

--- api/_lib/db.js
+++ api/_lib/db.js
@@ -63,7 +63,7 @@ export function normalizeName(name) {
 export function shuffle(arr) {
   const a = [...arr];
   for (let i = a.length - 1; i > 0; i--) {
-    const j = crypto.randomInt(i + 1);
+    const j = Math.floor(Math.random() * (i + 1));
     [a[i], a[j]] = [a[j], a[i]];
   }
   return a;
